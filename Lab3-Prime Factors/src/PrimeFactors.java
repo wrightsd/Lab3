6e1627561fdce89dd1f02d6f467c12160f84a2da
wrightsd <wrightsd@rose-hulman.edu>
2015-03-24 14:21:32 -0400

    Test of One passes

---
+++ Lab3-Prime Factors/src/PrimeFactors.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PrimeFactors {
+
+	public static List<Integer> generate(int i) {
+		return new ArrayList<Integer>();
+	}
+
+}
--- Lab3-Prime Factors/src/PrimeFactorsTesting.java
+++ Lab3-Prime Factors/src/PrimeFactorsTesting.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 
@@ -7,7 +10,11 @@ public class PrimeFactorsTesting {
 
 	@Test
 	public void testOne() {
-		assertEquals(list[]	, PrimeFactors.generate(1));
+		assertEquals(list()	, PrimeFactors.generate(1));
+	}
+	
+	public List<Integer> list(){
+		return Arrays.asList();
 	}
 
 }
