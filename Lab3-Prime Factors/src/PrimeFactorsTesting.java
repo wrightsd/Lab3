4eeadf1d50416cfecc4ee5df5402510275583894
wrightsd <wrightsd@rose-hulman.edu>
2015-03-24 14:12:03 -0400

    Test of One created

---
+++ Lab3-Prime Factors/src/PrimeFactorsTesting.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class PrimeFactorsTesting {
+
+	@Test
+	public void testOne() {
+		assertEquals(list[]	, PrimeFactors.generate(1));
+	}
+
+}
